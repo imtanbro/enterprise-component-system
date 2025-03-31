57302df438cd13e833dd55eb9c8a44c6d271ed31
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2025-03-31 23:58:04 +0530

    Constant routes

---
+++ src/Common/Constants/Routes.tsx
@@ -0,0 +1,7 @@
+export const HOME_PAGE = "/";
+export const DASHBOARD = "/dashboard";
+export const MULTI_STEP_FORM = "/multi-step-form";  
+export const HIERARCHICAL_TREE = "/hierarchical-tree";  
+export const DATA_GRID = "/data-grid";  
+export const CONTACT = "/contact";  
+export const ERROR_PAGE = "*";  
